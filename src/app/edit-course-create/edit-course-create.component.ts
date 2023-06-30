0c1289c489894021cac0ec6d8ce4f7aa50f0f9a2
Jyothika <jyothikasivasai@gmail.com>
2023-06-30 18:23:39 +0800

    Give schedule page buttons styling

--- src/app/edit-course-create/edit-course-create.component.ts
+++ src/app/edit-course-create/edit-course-create.component.ts
@@ -9,4 +9,9 @@ export class EditCourseCreateComponent {
   mcitems = ["Aviation Management", "Electronic Personal Communication", "OPS Instrument",
 "Aviation Security", "Airport Fire Safety"]
   scitems = ["Aviation", "Management" ]
+  next = false;
+
+  moveNext() {
+    this.next = !this.next;
+  }
 }
