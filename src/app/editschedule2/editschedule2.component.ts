ca0c7738e33ddf3d2528b70909055e9bd6548832
Jyothika <jyothikasivasai@gmail.com>
2023-07-01 02:28:55 +0800

    Change header text to match Figma mockup

--- src/app/editschedule2/editschedule2.component.ts
+++ src/app/editschedule2/editschedule2.component.ts
@@ -9,36 +9,9 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./editschedule2.component.css']
 })
 export class Editschedule2Component {
-<<<<<<< Updated upstream
-  
-      
-  
-  selectedOption1: string | undefined;
-  selectedOption2: string | undefined;
-  // public date = new Date();
-  // name = new FormControl();
-  // dropdown = new FormControl();
-  // options = ['Select', 
-  // 'Database Lab',
-  // 'test lab-1', 
-  // 'test lab-1', 
-  // 'test lab-1'];
-  // testSubscription: Subscription | undefined;
-
-  // public calendarDate = new Date(2022, 2, 1);
-  // public disabledDates = [
-  //   [new Date(2022, 2, 4), new Date(2022, 2, 7)], // range of dates that cannot be selected
-  //   new Date(2022, 2, 16), // single date that cannot be selected
-  //   new Date(2022, 3, 16),
-  //   [new Date(2022, 4, 2), new Date(2022, 4, 8)],
-  // ]
-  // public maxDate = new Date(2022, 5, 0);
-  // public minDate = new Date(2022, 0, 1);
-=======
   headeritems = ['Start Date', 'End Date', 'Instructor', 'Lab']
   items = ['', '']
   index = 0;
->>>>>>> Stashed changes
 
   deleteItem(i:number) {
     this.items.splice(i, 1);
