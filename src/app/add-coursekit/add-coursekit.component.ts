af9904cc91973aafe95396ab1c84aa5dc28e7e56
Jyothika <jyothikasivasai@gmail.com>
2023-07-01 17:26:50 +0800

    Add custom dropdown for editschedule2

---
+++ src/app/add-coursekit/add-coursekit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-coursekit',
+  templateUrl: './add-coursekit.component.html',
+  styleUrls: ['./add-coursekit.component.css']
+})
+export class AddCoursekitComponent {
+
+}
