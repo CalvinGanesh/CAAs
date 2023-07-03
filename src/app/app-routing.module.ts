47f6581333a5187f0bfe1076418cfe1e276889d5
Jyothika <jyothikasivasai@gmail.com>
2023-07-03 14:47:53 +0800

    Add routing to coursekit and course approval changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { ScheduleComponent } from './schedule/schedule.component';
 import { RouterModule, Routes } from '@angular/router';
 import { EditCourseCreateComponent } from './edit-course-create/edit-course-create.component';
+import { CourseApprovalComponent } from './course-approval/course-approval.component';
+import { CoursekitComponent } from './coursekit/coursekit.component';
 
 const routes: Routes = [
   {path: 'Schedule Class', component: ScheduleComponent},
-  {path: 'Course', component: EditCourseCreateComponent}
+  {path: 'Schedule Class/List', component: ScheduleComponent},
+  {path: 'Course', component: EditCourseCreateComponent},
+  {path: 'Course/Course Approval', component: CourseApprovalComponent},
+  {path: 'Course/Course Kit', component: CoursekitComponent}
 ];
 
 @NgModule({
--- src/app/course-approval/course-approval.component.ts
+++ src/app/course-approval/course-approval.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./course-approval.component.css']
 })
 export class CourseApprovalComponent {
-
+  headeritems = ['Course Name', 'Course Code', 'Main Category', 'Sub Category', 'Fee', 'Action'];
+  items = [{coursename: 'Air Navigation', coursecode: 'AN9191', mc: 'Navigation', sc: 'Air Navigation Service', fee: '2500'}];
 }
